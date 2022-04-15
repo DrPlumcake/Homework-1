fe7d7deb59ea43627a0e4cda454683367a1ea0c2
Andrea Fantasticini <86974340+DrPlumcake@users.noreply.github.com>
2022-04-15 16:56:39 +0200

    Add files via upload

---
+++ src/it/uniroma3/siw/model/Docente.java
@@ -0,0 +1,93 @@
+package it.uniroma3.siw.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Docente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String cognome;
+	
+	@Column
+	private LocalDate dataDiNascita;
+	
+	@Column
+	private String luogoDiNascita;
+	
+	@Column
+	private int IVA;
+	/*
+	 * Metto nel database tutti i corsi di un docente. 
+	 * Assumo che non sia possibile effettuare un cambio di cattedra; tolto il docente, vengono tolti anche tutti i suoi corsi.
+	 */
+	@OneToMany (mappedBy = "curatore", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	private List<Corso> courses;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public LocalDate getDataDiNascita() {
+		return dataDiNascita;
+	}
+
+	public void setDataDiNascita(LocalDate dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
+	public String getLuogoDiNascita() {
+		return luogoDiNascita;
+	}
+
+	public void setLuogoDiNascita(String luogoDiNascita) {
+		this.luogoDiNascita = luogoDiNascita;
+	}
+
+	public int getIVA() {
+		return IVA;
+	}
+
+	public void setIVA(int iVA) {
+		IVA = iVA;
+	}
+	
+	
+	
+}
